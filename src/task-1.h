fcb5ce5e4905a6990487307cfddd107735277f6a
kamronbek-olimov-1500196 <127713658+kamronbek-olimov-1500196@users.noreply.github.com>
2023-10-12 14:50:54 +0500

    Update task-1.h

--- src/task-1.h
+++ src/task-1.h
@@ -7,6 +7,12 @@
 class Summation {
 private:
     int a, b, c;
+    int sum(int x, int y) {
+        a = x;
+        b = y;
+        c = a + b; 
+    }
+
 public:
     int sum(int x, int y) {
         a = x;
